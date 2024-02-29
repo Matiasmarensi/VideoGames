d35fb877cc4750394b9efc0a89aef35f841029b1
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-02-29 01:07:51 -0300

    json node

--- api/src/db.js
+++ api/src/db.js
@@ -12,6 +12,13 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+//   logging: false,
+//   native: false,
+//   dialectOptions: {
+//     ssl: false,
+//   },
+// });
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
